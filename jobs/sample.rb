0a27b53771a0f9c945269c501b306e9f05d33524
Alexis Reigel <mail@koffeinfrei.org>
2015-07-04 16:49:57 +0200

    employees and contractors

--- jobs/sample.rb
+++ jobs/sample.rb
@@ -1,13 +1,13 @@
-current_performance = 0
-current_karma = 0
-
 SCHEDULER.every '2s' do
   last_performance = 0.65
   current_performance = 0.72
-  last_karma     = current_karma
-  current_karma     = rand(200000)
+  last_employees = 25
+  current_employees = 25
+  last_contractors = 25
+  current_contractors = 5
 
   send_event('salary-performance', { current: current_performance, last: last_performance })
-  send_event('karma', { current: current_karma, last: last_karma })
+  send_event('employees', { current: current_employees, last: last_employees })
+  send_event('contractors', { current: current_contractors, last: last_contractors })
   send_event('code-coverage',   { value: 97.8 })
 end
