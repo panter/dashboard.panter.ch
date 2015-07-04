529e5f6f6f2ad27daeb0b48c5d4e6e62a8bc9365
Alexis Reigel <mail@koffeinfrei.org>
2015-07-04 16:29:44 +0200

    salary performance

--- jobs/sample.rb
+++ jobs/sample.rb
@@ -1,13 +1,13 @@
-current_valuation = 0
+current_performance = 0
 current_karma = 0
 
 SCHEDULER.every '2s' do
-  last_valuation = current_valuation
+  last_performance = 0.65
+  current_performance = 0.72
   last_karma     = current_karma
-  current_valuation = rand(100)
   current_karma     = rand(200000)
 
-  send_event('valuation', { current: current_valuation, last: last_valuation })
+  send_event('salary-performance', { current: current_performance, last: last_performance })
   send_event('karma', { current: current_karma, last: last_karma })
   send_event('code-coverage',   { value: 97.8 })
 end
