982b564aea28295d3ffa30147275e7c202433882
Alexis Reigel <mail@koffeinfrei.org>
2015-07-05 22:58:47 +0200

    paginate github requests

--- jobs/github.rb
+++ jobs/github.rb
@@ -3,8 +3,16 @@ require 'octokit'
 client = Octokit::Client.new(:login => ENV['GITHUB_LOGIN'], :password => ENV['GITHUB_PASSWORD'])
 
 SCHEDULER.every '10m' do
-  commits = client.organization_events('panter', per_page: 1000)
-    .select { |x| x.type == 'PushEvent' && x.created_at.to_date == Date.today }
+  events = client.organization_events('panter')
+
+  # get all today's events
+  while events.last.created_at.to_date == Date.today
+    events << client.organization_events('panter')
+  end
+  events.select! { |event| event.created_at.to_date == Date.today }
+
+  commits = events
+    .select { |event| event.type == 'PushEvent' }
     .map(&:payload)
     .flat_map(&:commits)
     .length
