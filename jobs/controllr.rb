23a1b7212bc1b91ab4a639383b7352c381e0fc38
Alexis Reigel <mail@koffeinfrei.org>
2015-07-07 12:12:40 +0200

    get performance from controllr

--- jobs/controllr.rb
+++ jobs/controllr.rb
@@ -1,12 +1,24 @@
 require 'json'
 
 def user_count(employment)
-  users_url = "http://controllr.panter.biz/users.json?&iDisplayLength=1000&search[active_in][]=true&search[employment_in][]=%{employment}&user_token=%{token}"
+  api_url = "http://controllr.panter.biz/users.json?&iDisplayLength=1000&search[active_in][]=true&search[employment_in][]=%{employment}&user_token=%{token}"
 
-  url = format(users_url, employment: employment, token: ENV['CONTROLLR_TOKEN'])
+  url = format(api_url, employment: employment, token: ENV['CONTROLLR_TOKEN'])
   json = Net::HTTP.get(URI(url))
   data = JSON.parse(json)
   data.fetch('iTotalDisplayRecords', 0)
+end
+
+def performance(month)
+  api_url = 'http://controllr.panter.biz/api/monthly_salaries/spreadsheet_data.json?month=%{month}&user_token=%{token}'
+  url = format(api_url, month: month, token: ENV['CONTROLLR_TOKEN'])
+  json = Net::HTTP.get(URI(url))
+  data = JSON.parse(json)
+  data_totals = data['totals']
+
+  performance = data_totals['internal_hours_billable'].to_f / data_totals['internal_hours_worked'].to_f
+
+  performance.round(2)
 end
 
 def update
@@ -15,6 +27,12 @@ def update
 
   send_event('employees', { current: employees })
   send_event('contractors', { current: contractors })
+
+  last_performance = performance(5)
+  current_performance = performance(6)
+
+  send_event('salary-performance', { current: current_performance, last: last_performance })
+  send_event('code-coverage',   { value: 97.8 })
 end
 
 update
--- jobs/sample.rb
+++
@@ -1,7 +0,0 @@
-SCHEDULER.every '2s' do
-  last_performance = 0.65
-  current_performance = 0.72
-
-  send_event('salary-performance', { current: current_performance, last: last_performance })
-  send_event('code-coverage',   { value: 97.8 })
-end
