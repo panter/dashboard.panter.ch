5a53f7ed3d1f239607d31f717f2d7d8f01298845
Alexis Reigel <mail@koffeinfrei.org>
2015-12-08 22:32:50 +0100

    update with the correct remote

--- lib/git_repository_cloner.rb
+++ lib/git_repository_cloner.rb
@@ -38,15 +38,27 @@ class GitRepositoryCloner
 
     repositories.each do |repository|
       target_directory = "repositories/#{repository.name}"
+      remote_url = repository.send(clone_method)
 
       if Dir.exist?(target_directory)
-        puts "Updating #{repository.name}"
-        repository = Rugged::Repository.new(target_directory)
-        repository.fetch('origin', credentials: credentials)
+        local_repository = Rugged::Repository.new(target_directory)
+        origin = local_repository.remotes.select { |remote| remote.name == 'origin' }.first
+
+        # make sure we don't mix up github and gitlab repositories
+        # (this may be the case when e.g. a github repo is transferred
+        # to gitlab, while the github repository is left in place. in
+        # this scenario, we may be trying to fetch with the wrong
+        # credentials)
+        if origin && origin.url == remote_url
+          puts "Updating #{repository.name}"
+          local_repository.fetch('origin', credentials: credentials)
+        else
+          puts "Not updating #{repository.name}, we have the wrong remote (#{remote_url})."
+        end
       else
         puts "Cloning #{repository.name}"
         Rugged::Repository.clone_at(
-          repository.send(clone_method),
+          remote_url,
           target_directory,
           bare: true,
           credentials: credentials
