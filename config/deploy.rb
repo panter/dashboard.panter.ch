c5bdda54d29b74f36d7fbc8c15375fb6c0cb9a57
Markus Koller <markus-koller@gmx.ch>
2016-09-15 22:04:48 +0200

    Update Capistrano lock version

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,5 +1,5 @@
 # config valid only for current version of Capistrano
-lock '3.4.0'
+lock '3.5.0'
 
 set :application, 'dashboard.panter.ch'
 set :repo_url, 'git@github.com:panter/dashboard.panter.ch.git'
